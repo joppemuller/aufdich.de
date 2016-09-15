2d60bb7948ef49a6d87e277dc6706aa2922e9832
joppemuller <joppe@thekids.eu>
2016-09-15 18:59:34 +0200

    V2

--- js/game.js
+++ js/game.js
@@ -1,9 +1,8 @@
 // Create the canvas
-var canvas = document.createElement("canvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
-document.body.appendChild(canvas);
 
 // Background image
 var bgReady = false;
